2e77842e3cbbe6c8a652987210a6c1064abfb6d9
Atin Malaviya <atin.malaviya@gmail.com>
2010-10-12 23:26:36 -0400

    First checkin for multistage initramfs loading.

---
+++ stage1/init_logger.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec /stage1.sh >> /stage1.log 2>&1
---
+++ stage1/stage1.sh
@@ -0,0 +1,170 @@
+#!/bin/sh
+###############################################################################
+#                                                                             #
+#    Stage 1 initramfs loader for Samsung Galaxy S                            #
+#                                                                             #
+#    Devices supported and tested :                                           #
+#      o AT&T Captivate                                                       #
+#                                                                             #
+#    Credits:                                                                 #
+#      Atin Malaviya (atinm @ xda-developers)                                 #
+#      Francois Simond *supercurio @ xda-developers)                          #
+#                                                                             #
+#    Released under the GPLv3                                                 #
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
+#                                                                             #
+###############################################################################
+set -x
+PATH=/bin:/sbin:/usr/bin/:/usr/sbin:/system/bin:/system/sbin
+
+alias mount_sdcard="mount -t vfat -o utf8 /dev/block/mmcblk0p1 /sdcard"
+alias mount_system="mount -t rfs -o ro,check=no /dev/block/stl9 /system"
+debug_mode=1
+
+load_stage() {
+    # don't reload a stage already in memory
+    if ! test -f /tmp/stage$1_loaded; then
+	case $1 in
+	    *)
+		stagefile="/sdcard/init/stage$1.cpio.lzma"
+
+		if test -f $stagefile ; then
+		    # load the designated stage after verifying it's
+		    # signature to prevent security exploit from sdcard
+		    signature=`sha1sum $stagefile | cut -d' ' -f 1`
+		    for x in `cat /res/signatures/$1.sig`; do
+			if test "$x" = "$signature"  ; then
+			    log "load stage $1 from SD"
+			    lzcat -dc $stagefile | cpio -div
+			    echo 1 > /tmp/stage$1_loaded
+			    return
+			fi
+		    done
+		    log "stage $1 not loaded, signature mismatch"
+		else
+		    log "stage $1 not loaded, $stagefile not found"
+		fi
+
+		;;
+	esac
+    fi
+}
+
+log() {
+    log="stage1.sh: $1"
+    echo `date '+%Y-%m-%d %H:%M:%S'` $log >> /stage1.log
+}
+
+letsgo() {
+    # dump logs to the sdcard
+    if test -d /sdcard/init ; then
+	log "running stage1 init !"
+	
+	if test $debug_mode = 1; then
+	    cat /stage1.log > /sdcard/init/stage1-init-"`date '+%Y-%m-%d_%H-%M-%S'`".log
+	fi
+    fi
+
+    umount /sdcard
+    umount /system
+
+    # if test -f /tmp/stage$1_loaded ; then
+    #     # run the next stage init
+    # 	exec stage$1.sh
+
+    # else
+	# clean up /sdcard mount point
+	rmdir /sdcard 
+
+        # run Samsung's init and disappear
+        exec /sbin/init
+    # fi
+}
+
+setup_devices() {
+    # proc and sys are  used 
+    mount -t proc proc /proc
+    mount -t sysfs sys /sys
+
+    # create used devices nodes
+    mkdir /dev/block
+
+    # create used devices nodes
+    # standard
+    mknod /dev/null c 1 3
+    mknod /dev/zero c 1 5
+
+    # internal & external SD
+    mknod /dev/block/mmcblk0 b 179 0
+    mknod /dev/block/mmcblk0p1 b 179 1
+    mknod /dev/block/mmcblk0p2 b 179 2
+    mknod /dev/block/stl1 b 138 1
+    mknod /dev/block/stl2 b 138 2
+    mknod /dev/block/stl3 b 138 3
+    mknod /dev/block/stl4 b 138 4
+    mknod /dev/block/stl5 b 138 5
+    mknod /dev/block/stl6 b 138 6
+    mknod /dev/block/stl7 b 138 7
+    mknod /dev/block/stl8 b 138 8
+    mknod /dev/block/stl9 b 138 9
+    mknod /dev/block/stl10 b 138 10
+    mknod /dev/block/stl11 b 138 11
+    mknod /dev/block/stl12 b 138 12
+
+    # ko files for 3D
+    insmod /modules/pvrsrvkm.ko
+    insmod /modules/s3c_lcd.ko
+    insmod /modules/s3c_bc.ko
+
+    # ko files for vibrator
+    insmod /lib/modules/vibrator.ko
+
+    # ko files for Fm radio
+    insmod /lib/modules/Si4709_driver.ko
+
+    insmod /lib/modules/fsr.ko
+    insmod /lib/modules/fsr_stl.ko
+    insmod /lib/modules/rfs_glue.ko
+    insmod /lib/modules/rfs_fat.ko
+
+# parameter block
+    insmod /lib/modules/j4fs.ko
+    insmod /lib/modules/param.ko
+
+# mount modules
+    insmod /lib/modules/onedram.ko
+    insmod /lib/modules/svnet.ko
+    insmod /lib/modules/modemctl.ko
+    insmod /lib/modules/storage.ko
+    insmod /lib/modules/bthid.ko
+
+    # new in beta5, using /system
+    mount_system
+    mount_sdcard
+}
+
+#mknod and insmod the things we need
+setup_devices
+
+# debug mode detection
+if test -f /sdcard/init/enable-debug ; then
+    debug_mode=1
+fi
+
+# STAGE 2
+load_stage 2
+
+# run stage2's init or samsung's init on failure
+letsgo 2
